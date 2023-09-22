6cf4eb0297a3a5b2a214f08fe394290f5d1984a4
Stuart Spackman <stuart.spackman@gmail.com>
2023-09-22 07:15:02 -0700

    correcting operations buttons

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function Calculator() {
         '+': (a, b) => a + b,
         '-': (a, b) => a - b,
         '*': (a, b) => a * b,
-        '/': (a, b) => a / b
+        '÷': (a, b) => a / b
     };
     this.calculate = function (string) {
         let split = string.split(' '),
@@ -15,45 +15,44 @@ function Calculator() {
 }
 let calc = new Calculator();
 let calcStack = [];
+let opButtonWasJustPushed = false;
 
+
+function update(newValue) {
+    if (newValue.length <= 13) {
+        display.innerText = newValue;
+    } else if (newValue.length > 13) {
+        display.innerText = 'overflow - hit the clear button';
+    }
+}
 
 const display = document.querySelector('.display');
-const displayContent = document.createElement('span');
-display.appendChild(displayContent);
-displayContent.innerText = '';
+display.innerText = '';
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-    displayContent.innerText = '';
+    display.innerText = '';
     calcStack = [];
 });
 
 const plusMinusButton = document.querySelector('.plus-minus');
 plusMinusButton.addEventListener('click', () => {
-    if (displayContent.innerText !== '') {
-        let currentText = displayContent.innerText;
+    if (display.innerText !== '') {
+        let currentText = display.innerText;
         let currentValue = Number(currentText);
         let newValue = (currentValue * -1).toString();
-        if (newValue.length <= 13) {
-            displayContent.innerText = newValue;
-        } else if (newValue.length > 13) {
-            displayContent.innerText = 'overflow - hit the clear button';
-        }
+        update(newValue);
     }
 });
 
 const percentButton = document.querySelector('.percent');
 percentButton.addEventListener('click', () => {
-    if (displayContent.innerText !== '') {
-        let currentValue = Number(displayContent.innerText);
+    if (display.innerText !== '') {
+        let currentValue = Number(display.innerText);
         let newValue = (currentValue / 100);
-        newValue = (Math.round(newValue * 100000)) / 100000;
+        newValue = (Math.round(newValue * 1000000)) / 1000000;
         newValue = newValue.toString();
-        if (newValue.length <= 13) {
-            displayContent.innerText = newValue;
-        } else if (newValue.length > 13) {
-            displayContent.innerText = 'overflow - hit the clear button';
-        }
+        update(newValue);
     }
 });
 
@@ -61,12 +60,13 @@ const numberButtons = document.querySelectorAll('.number');
 for (button of numberButtons) {
     let number = button.innerText;
     button.addEventListener('click', () => {
-        if (displayContent.innerText !== '0') {
-            let newValue = displayContent.innerText + number;
-            if (newValue.length <= 13) {
-                displayContent.innerText = newValue;
-            } else if (newValue.length > 13) {
-                displayContent.innerText = 'overflow - hit the clear button';
+        if (opButtonWasJustPushed === true) {
+            display.innerText = number;
+            opButtonWasJustPushed = false;
+        } else {
+            if (display.innerText !== '0') {
+                let newValue = display.innerText + number;
+                update(newValue);
             }
         }
     });
@@ -75,30 +75,23 @@ for (button of numberButtons) {
 const zeroButton = document.querySelector('.zero');
 zeroButton.addEventListener('click', () => {
     let zero = zeroButton.innerText;
-    if (display.innerText[0] === '0') {
+    if (display.innerText[display.innerText.length - 1] === '0' && !display.innerText.includes('.')) {
+        console.log('testing');
         return;
     } else {
-        let newValue = displayContent.innerText + zero;
-        if (newValue.length <= 13) {
-            displayContent.innerText = newValue;
-        } else if (newValue.length > 13) {
-            displayContent.innerText = 'overflow - hit the clear button';
-        }
+        let newValue = display.innerText + zero;
+        update(newValue);
     }
 });
 
 const decimalButton = document.querySelector('.decimal');
 decimalButton.addEventListener('click', () => {
     let decimal = decimalButton.innerText;
-    if (displayContent.innerText.includes('.')) {
+    if (display.innerText.includes('.')) {
         return;
     } else {
-        let newValue = displayContent.innerText + decimal;
-        if (newValue.length <= 13) {
-            displayContent.innerText = newValue;
-        } else if (newValue.length > 13) {
-            displayContent.innerText = 'overflow - hit the clear button';
-        }
+        let newValue = display.innerText + decimal;
+        update(newValue);
     }
 });
 
@@ -107,9 +100,9 @@ for (button of opButtons) {
     let operation = button.innerText;
     button.addEventListener('click', () => {
         if (calcStack.length === 0) {
-            calcStack.push(displayContent.innerText);
-            displayContent.innerText = '';
+            calcStack.push(display.innerText);
             calcStack.push(operation);
+            opButtonWasJustPushed = true;
         }
     });
 }
@@ -117,20 +110,24 @@ for (button of opButtons) {
 const equalsButton = document.querySelector('.equals');
 equalsButton.addEventListener('click', () => {
     if (calcStack.length === 2) {
-        calcStack.push(displayContent.innerText);
+        calcStack.push(display.innerText);
         let calcString = calcStack.join(' ');
         calcStack = [];
         if (calcString === '0 / 0') {
-            displayContent.innerText = 'I knew you would try that.';
+            display.innerText = 'I knew you would try that.';
             return;
         }
         let newValue = calc.calculate(calcString);
         newValue = (Math.round(newValue * 100000)) / 100000;
         newValue = newValue.toString();
-        if (newValue.length <= 13) {
-            displayContent.innerText = newValue;
-        } else if (newValue.length > 13) {
-            displayContent.innerText = 'overflow - hit the clear button';
-        }
+        update(newValue);
+    }
+});
+
+const deleteButton = document.querySelector('.delete');
+deleteButton.addEventListener('click', () => {
+    if (display.innerText === '') return;
+    else {
+        display.innerText = display.innerText.substr(0, display.innerText.length - 1);
     }
 });
